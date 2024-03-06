5c3ccec7aaf8819c2f6271bc9f87ccaea0253242
a-marii <81440255+a-marii@users.noreply.github.com>
2024-03-06 16:33:08 +0100

    VMC split

---
+++ Model/Model.js
@@ -0,0 +1,89 @@
+// ===========================================================================
+// The model of the application. Note that this is not the same thing as
+// the dictionary. The model keeps track of the information that is involved
+// in the activities in the application, and which, most of them, can change
+// overtime.
+//
+// The data can be stored in the model in many different ways. In this file
+// it is stored in simple variables, but for larger programs it is better
+// to use a dictionary or other data structure. If we want to save the model,
+// it will be tedious to ensure that all the individual variables are saved
+// (and restored), whereas a data structure can be saved as a single entity
+// in one action. It will also be easier when we use the local storage to
+// save data over page refreshes and sessions using Local storage.
+//
+// ===========================================================================
+
+var modelData = {};
+
+modelData['orders'] = [];
+modelData['products'] = [];
+modelData['productAttributes'] = ['productID', 'productName', 'price', 'stock']
+
+// Example of orders
+
+var DB_order_example = [
+    {
+        "order_id": 10000,
+        "bartender_id": "B00000",
+        "customer_id": "C00001",
+        "order":[],
+        "quantities":[],
+        "amount": 2153,
+        'tableNr' : 7,
+        "timestamp": "2023-11-10 19:04:13",
+        "suborder":[[["25053", 2], ["190719", 1]],[["51029", 1]],[["25053", 2]]],
+        "subpay":[1079,190,884],
+        "paid":false
+    },
+    {
+        "order_id": 10001,
+        "bartender_id": "B00000",
+        "customer_id": "C00002",
+        "order":[],
+        "quantities":[],
+        "amount": 1079,
+        'tableNr' : 5,
+        "timestamp": "2023-11-10 19:36:43",
+        "suborder":[[["25053", 2], ["190719", 1]]],
+        "subpay":[1079],
+        "paid":false
+    }
+   
+]
+
+modelData['orders'] = DB_order_example;
+
+// Example of products (This will use the same as menu)
+var productExample1 = {
+    'productID' : "25053",
+    'productName' : "Braastad XO",
+    'price' : "442.00",
+    'stock': 17,
+    'available': false
+}
+var productExample2 = {
+    'productID' : "190719",
+    'productName' : "Vanlig Vodka",
+    'price' : "195.00",
+    'stock': 9,
+    'available': true
+}
+var productExample3 = {
+    'productID' : "51029",
+    'productName' : "Dworek Vodka",
+    'price' : "190.00",
+    'stock': 9,
+    'available': true
+}
+modelData['products'].push(productExample1, productExample2,productExample3 );
+
+
+// The model in this version can then easily be transferred into the Local
+// or Session storage. The only problem is that local storage does not store
+// other data types than strings, so there is a need for conversion back to
+// other data types upon retrieving the data from the storage.
+//
+// ===========================================================================
+// END OF FILE
+// ===========================================================================
---
+++ Model/db_model.js
@@ -0,0 +1,213 @@
+var DB = require('./DBLoaded.js');
+var DB2 = require('./Beverages.js');
+var DB3 = require('./Dishes.js');
+
+function getAllUsers() {
+    // Create an empty array
+    var allUsers = [];
+    // Iterate over the database
+    for (i = 0; i < DB.users.length; i++) {
+        allUsers.push(DB.users[i]);
+    }
+    // Return the array
+    return allUsers;
+}
+
+function getAllUserNames() {
+    var nameCollect = [];
+    for (i = 0; i < DB.users.length; i++) {
+        nameCollect.push(DB.users[i].username);
+    }
+    return nameCollect;
+}
+
+function getCredits(userId) {
+    // Find the user in the database
+    var account = DB.account.find(function(account) {
+        return account.user_id == userId;
+    });
+    return account.creditSEK
+}
+
+function increaseCredits(userId, creditsToAdd) {
+    // Find the user in the database
+    var user = DB.account.find(function(user) {
+        return user.user_id == userId;
+    });
+
+    // If the user is found, increase their credits
+    if (user) {
+        // Convert creditSEK to number, add creditsToAdd, and convert back to string
+        var currentCredits = parseInt(user.creditSEK);
+        var newCredits = currentCredits + parseInt(creditsToAdd);
+        user.creditSEK = newCredits.toString();
+        return true; // Return true indicating the operation was successful
+    } else {
+        return false; // Return false indicating the user was not found
+    }
+}
+
+function decreaseCredits(userId, creditsToSubtract) {
+    // Find the user in the database
+    var user = DB.account.find(function(user) {
+        return user.user_id == userId;
+    });
+
+    // If the user is found, increase their credits
+    if (user) {
+        // Convert creditSEK to number, add creditsToSubtract, and convert back to string
+        var currentCredits = parseInt(user.creditSEK);
+        var newCredits = currentCredits - parseInt(creditsToSubtract);
+        user.creditSEK = newCredits.toString();
+        return true; // Return true indicating the operation was successful
+    } else {
+        return false; // Return false indicating the user was not found
+    }
+}
+
+function getUser(userId) {
+    // Find the user in the database
+    var user = DB.users.find(function(user) {
+        return user.user_id == userId;
+    });
+    return user
+}
+
+// Function to update stock of a specific item
+function updateStock(itemNr, newStock) {
+    // Find the item in the beverages array
+    var item = DB2.beverages.find(function(item) {
+        return item.nr == itemNr;
+    });
+
+    // If the item is found, update its stock
+    if (item) {
+        item.stock = newStock.toString();
+        return true; // Return true indicating the operation was successful
+    } else {
+        return false; // Return false indicating the item was not found
+    }
+}
+
+// Function to decrease stock by one
+function decreaseStock(itemNr) {
+    // Find the item in the beverages array
+    var item = DB2.beverages.find(function(item) {
+        return item.nr == itemNr;
+    });
+
+    // If the item is found, update its stock
+    if (item) {
+        item.stock = item.stock-1
+        return true; // Return true indicating the operation was successful
+    } else {
+        return false; // Return false indicating the item was not found
+    }
+}
+
+function getBeverage(itemNr) {
+    // Find the item in the beverages array
+    var item = DB2.beverages.find(function(item) {
+        return item.nr == itemNr;
+    });
+    return item
+}
+
+function getAllBeverages() {
+    // Create an empty array
+    var allBeverages = [];
+    // Iterate over the database
+    for (i = 0; i < DB2.beverages.length; i++) {
+        allBeverages.push(DB2.beverages[i]);
+    }
+    // Return the array
+    return allBeverages;
+}
+
+function getDish(itemNr) {
+    // Find the item in the dishes array
+    var item = DB3.dishes.find(function(item) {
+        return item.nr == itemNr;
+    });
+    return item
+}
+
+function getAllDishes() {
+    // Create an empty array
+    var allDishes = [];
+    // Iterate over the database
+    for (i = 0; i < DB3.dishes.length; i++) {
+        allDishes.push(DB3.dishes[i]);
+    }
+    // Return the array
+    return allDishes;
+}
+
+function filterBeveragesByCategory(category) {
+    // Create an empty array
+    var filtered = [];
+    // Iterate over the database
+    for (i = 0; i < DB2.beverages.length; i++) {
+        if (DB2.beverages[i].category==category)
+        filtered.push(DB2.beverages[i]);
+    }
+    // Return the array
+    return filtered;
+}
+
+function filterDishesByCategory(category) {
+    // Create an empty array
+    var filtered = [];
+    // Iterate over the database
+    for (i = 0; i < DB3.dishes.length; i++) {
+        if (DB3.dishes[i].category==category)
+        filtered.push(DB3.dishes[i]);
+    }
+    // Return the array
+    return filtered;
+}
+
+function filterBeveragesByName(name) {
+    // Create an empty array
+    var filtered = [];
+    // Iterate over the database
+    for (i = 0; i < DB2.beverages.length; i++) {
+        if (DB2.beverages[i].namn.toLowerCase().includes(name.toLowerCase()))
+        filtered.push(DB2.beverages[i]);
+    }
+    // Return the array
+    return filtered;
+}
+
+function filterDishesByName(name) {
+    // Create an empty array
+    var filtered = [];
+    // Iterate over the database
+    for (i = 0; i < DB3.dishes.length; i++) {
+        if (DB3.dishes[i].name.toLowerCase().includes(name.toLowerCase()))
+        filtered.push(DB3.dishes[i]);
+    }
+    // Return the array
+    return filtered;
+}
+
+function filterDishesByName(name) {
+    // Create an empty array
+    var filtered = [];
+    // Iterate over the database
+    for (i = 0; i < DB3.dishes.length; i++) {
+        if (DB3.dishes[i].name.toLowerCase().includes(name.toLowerCase()))
+        filtered.push(DB3.dishes[i]);
+    }
+    // Return the array
+    return filtered;
+}
+
+function checkPassword(username, password){
+    var user = DB.users.find(function(user) {
+        return user.username == username;
+    });
+    if (user.password==password)
+        return true
+    else return false
+}
