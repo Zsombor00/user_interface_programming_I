91a2fd3c9b0225d09b81fa4c8f69421d13c1fb42
Vanessa Wong <118968794+vanwongg@users.noreply.github.com>
2024-03-17 03:47:59 +0100

    Update Dishes.js

--- Model/Dishes.js
+++ Model/Dishes.js
@@ -4,99 +4,127 @@ var DB3 = {
             "nr": "1",
             "name": "Chips",
             "priceinclvat": "30.00",
-            "stock":"5",
-            "category":"Snack"
+            "stock":"50",
+            "category":"Snack",
+            "description":"Crispy and golden potato slices seasoned with a touch of salt. Perfectly fried to achieve that satisfying crunch. ",
+            "ingredients":"Potatoes, Salt. "
         }, {
             "nr": "2",
             "name": "Nuts",
             "priceinclvat": "40.00",
-            "stock":"3",
-            "category":"Snack"
+            "stock":"50",
+            "category":"Snack",
+            "description":"A delightful assortment of premium nuts, including almonds, walnuts, and cashews. Enjoy the natural crunch and rich flavors of these wholesome snacks. ",
+            "ingredients":"Almonds, Walnuts, Cashews. "
         }, {
             "nr": "3",
             "name": "Cheese Platter",
             "priceinclvat": "60.00",
             "stock": "50",
-            "category": "Appetizer"
+            "category": "Appetizer",
+            "description":"Indulge in a captivating selection of artisanal cheeses from around the world. This enticing platter features a variety of cheeses, accompanied by crispy crackers, fresh fruits, and savory nuts. ",
+            "ingredients":"Assorted Cheeses, Crackers, Fruits, Nuts. "
         },
-        {
-            "nr": "4",
-            "name": "Bruschetta",
-            "priceinclvat": "45.00",
-            "stock": "50",
-            "category": "Appetizer"
-        },
-        {
-            "nr": "5",
-            "name": "Caesar Salad",
-            "priceinclvat": "55.00",
-            "stock": "50",
-            "category": "Salad"
-        },
-        {
-            "nr": "6",
-            "name": "Margherita Pizza",
-            "priceinclvat": "70.00",
-            "stock": "50",
-            "category": "Pizza"
-        },
-        {
-            "nr": "7",
-            "name": "Spaghetti Carbonara",
-            "priceinclvat": "80.00",
-            "stock": "50",
-            "category": "Pasta"
-        },
-        {
-            "nr": "8",
-            "name": "Chicken Alfredo",
-            "priceinclvat": "85.00",
-            "stock": "50",
-            "category": "Pasta"
-        },
-        {
-            "nr": "9",
-            "name": "Grilled Salmon",
-            "priceinclvat": "90.00",
-            "stock": "50",
-            "category": "Seafood"
-        },
-        {
-            "nr": "10",
-            "name": "Beef Burger",
-            "priceinclvat": "75.00",
-            "stock": "50",
-            "category": "Burger"
-        },
-        {
-            "nr": "11",
-            "name": "Vegetable Stir-Fry",
-            "priceinclvat": "65.00",
-            "stock": "50",
-            "category": "Vegetarian"
-        },
-        {
-            "nr": "12",
-            "name": "Tiramisu",
-            "priceinclvat": "50.00",
-            "stock": "50",
-            "category": "Dessert"
-        },
-        {
-            "nr": "13",
-            "name": "Eggplant Parmesan",
-            "priceinclvat": "70.00",
-            "stock": "50",
-            "category": "Vegetarian"
-        },
-        {
-            "nr": "14",
-            "name": "Seafood Paella",
-            "priceinclvat": "85.00",
-            "stock": "50",
-            "category": "Seafood"
-        }
-    ]
-    };
+            {
+                "nr": "4",
+                "name": "Bruschetta",
+                "priceinclvat": "45.00",
+                "stock": "50",
+                "category": "Appetizer",
+                "description":"A true taste of Italy! Savor toasted bread slices topped with fresh tomatoes, aromatic basil, and a drizzle of olive oil. A delightful appetizer bursting with Mediterranean flavors. ",
+                "ingredients":"Bread, Tomatoes, Basil, Olive Oil. "
+            },
+            {
+                "nr": "5",
+                "name": "Caesar Salad",
+                "priceinclvat": "55.00",
+                "stock": "50",
+                "category": "Appetizer",
+                "description":"A classic salad featuring crisp romaine lettuce, crunchy croutons, and a generous sprinkle of Parmesan cheese. Tossed in a tangy Caesar dressing for a refreshing and satisfying experience. ",
+                "ingredients":"Romaine Lettuce, Croutons, Parmesan Cheese, Caesar Dressing. "
+            },
+            {
+                "nr": "6",
+                "name": "Margherita Pizza",
+                "priceinclvat": "70.00",
+                "stock": "50",
+                "category": "Snack",
+                "description":"Experience the simplicity and perfection of our Margherita Pizza. Topped with rich tomato sauce, creamy mozzarella cheese, and fragrant basil leaves on a thin, crispy crust. ",
+                "ingredients":"Pizza Dough, Tomato Sauce, Mozzarella Cheese, Basil. "
+            },
+            {
+                "nr": "7",
+                "name": "Spaghetti Carbonara",
+                "priceinclvat": "80.00",
+                "stock": "50",
+                "category": "Main",
+                "description":"An Italian favorite that will transport your taste buds. Enjoy al dente spaghetti coated in a velvety sauce made from eggs, Parmesan cheese, and crispy pancetta or bacon. ",
+                "ingredients":"Eggs, Parmesan Cheese, Bacon. "
+            },
+            {
+                "nr": "8",
+                "name": "Chicken Alfredo",
+                "priceinclvat": "85.00",
+                "stock": "50",
+                "category": "Main",
+                "description":"An irresistible pasta dish that combines tender grilled chicken with a luscious Alfredo sauce. The creamy blend of butter, heavy cream, Parmesan cheese, and garlic will leave you craving more. ",
+                "ingredients":"Fettuccine Pasta, Grilled Chicken, Alfredo Sauce. "
+            },
+            {
+                "nr": "9",
+                "name": "Grilled Salmon",
+                "priceinclvat": "90.00",
+                "stock": "50",
+                "category": "Main",
+                "description":"Delight in a perfectly grilled salmon fillet, seasoned with aromatic herbs and spices. Served with a side of vegetables, this dish offers a healthy and flavorful seafood experience. ",
+                "ingredients":"Salmon Fillet, Herbs, Spices, Vegetables. "
+            },
+            {
+                "nr": "10",
+                "name": "Beef Burger",
+                "priceinclvat": "75.00",
+                "stock": "50",
+                "category": "Snack",
+                "description":"Sink your teeth into a juicy beef patty nestled between fluffy burger buns. Topped with fresh lettuce, ripe tomato slices, onion, and your choice of cheese. Served with a side of crispy fries. ",
+                "ingredients":"Beef Patty, Burger Buns, Lettuce, Tomato, Onion, Cheese, Fries. "
+            },
+            {
+                "nr": "11",
+                "name": "Vegetable Stir-Fry",
+                "priceinclvat": "65.00",
+                "stock": "50",
+                "category": "Vegetarian",
+                "description":"A vibrant medley of fresh vegetables stir-fried to perfection. Enjoy the crispness and flavors of broccoli, bell peppers, carrots, and more, coated in a savory sauce. Served with steamed rice. ",
+                "ingredients":"Assorted Vegetables, Sauce, Steamed Rice. "
+            },
+            {
+                "nr": "12",
+                "name": "Tiramisu",
+                "priceinclvat": "50.00",
+                "stock": "50",
+                "category": "Dessert",
+                "description":"Treat yourself to a heavenly dessert with layers of delicate ladyfinger biscuits soaked in coffee and liqueur. Interlaced with a creamy mixture of mascarpone cheese, eggs, and sugar. Finished with a dusting of cocoa powder. ",
+                "ingredients":"Ladyfinger Biscuits, Coffee, Liqueur, Mascarpone Cheese, Eggs, Sugar, Cocoa Powder. "
+            },
+            {
+                "nr": "13",
+                "name": "Eggplant Parmesan",
+                "priceinclvat": "70.00",
+                "stock": "50",
+                "category": "Vegetarian",
+                "description":"Experience the flavors of Italy with our Eggplant Parmesan. Breaded and fried eggplant slices layered with tangy tomato sauce, melted mozzarella cheese, and grated Parmesan. Baked to perfection. ",
+                "ingredients":"Eggplant, Bread Crumbs, Tomato Sauce, Mozzarella Cheese, Parmesan Cheese. "
+            },
+            {
+                "nr": "14",
+                "name": "Seafood Paella",
+                "priceinclvat": "85.00",
+                "stock": "50",
+                "category": "Main",
+                "description":"Embark on a culinary journey to Spain with our tantalizing Seafood Paella. Saffron-infused rice cooked to perfection with an exquisite mix of shrimp, mussels, clams, and fish. Bursting with flavors and aromas. ",
+                "ingredients":"Rice, Saffron, Shrimp, Mussels, Clams, Fish, Spices. "
+            }
+        ]
+};
 
 module.exports = DB3;
